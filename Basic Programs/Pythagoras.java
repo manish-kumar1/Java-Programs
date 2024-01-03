514ce00e0054399db2778dbe52c5db30140818be
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-01-03 21:08:49 +0530

    Pythagoras Theorem

---
+++ Basic Programs/Pythagoras.java
@@ -0,0 +1,10 @@
+public class Pythagoras {
+    public static void main(String []args){
+        
+        int p = 3;
+        int b = 4;
+        double h;
+        h = Math.sqrt(Math.pow(p, 2)*Math.pow(b, 2));
+        System.out.println("Hypotenuse: "+h);
+    }
+}
